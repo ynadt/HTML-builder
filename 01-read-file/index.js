f2321afb398ff88a94d2507a82638a5da87ec0a3
ynadt <2769286@gmail.com>
2024-01-18 20:18:26 +0300

    feat: add solution for task 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+
+const readStream = fs.createReadStream(filePath);
+
+readStream.pipe(process.stdout);
+
+readStream.on('error', (error) => {
+  console.error('An error occurred:', error);
+});
